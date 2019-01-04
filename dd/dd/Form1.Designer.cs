a63a0abcb7f8e4fee8f927585b4172a34c158472
Duygu Çınar <duygucinar-1641@hotmail.com>
2019-01-04 10:31:07 +0300

    All files loaded.

---
+++ dd/dd/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace dd
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.width1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.goster = new System.Windows.Forms.Button();
+            this.dosya = new System.Windows.Forms.Button();
+            this.dosya_yolu = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.frame1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.format1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.height1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.sayi = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.groupBox1.Controls.Add(this.pic);
+            this.groupBox1.Location = new System.Drawing.Point(357, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(796, 680);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Görüntü";
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(6, 21);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(784, 653);
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Genişlik";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // width1
+            // 
+            this.width1.Location = new System.Drawing.Point(108, 37);
+            this.width1.Name = "width1";
+            this.width1.Size = new System.Drawing.Size(100, 22);
+            this.width1.TabIndex = 2;
+            this.width1.TextChanged += new System.EventHandler(this.width1_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.goster);
+            this.groupBox2.Controls.Add(this.dosya);
+            this.groupBox2.Controls.Add(this.dosya_yolu);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.frame1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.format1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.height1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.width1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(27, 189);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(299, 335);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Dosya Bilgileri";
+            // 
+            // goster
+            // 
+            this.goster.Location = new System.Drawing.Point(108, 293);
+            this.goster.Name = "goster";
+            this.goster.Size = new System.Drawing.Size(71, 27);
+            this.goster.TabIndex = 12;
+            this.goster.Text = "Göster";
+            this.goster.UseVisualStyleBackColor = true;
+            this.goster.Click += new System.EventHandler(this.goster_Click);
+            // 
+            // dosya
+            // 
+            this.dosya.Location = new System.Drawing.Point(108, 164);
+            this.dosya.Name = "dosya";
+            this.dosya.Size = new System.Drawing.Size(100, 28);
+            this.dosya.TabIndex = 11;
+            this.dosya.Text = "Dosya Seç";
+            this.dosya.UseVisualStyleBackColor = true;
+            this.dosya.Click += new System.EventHandler(this.dosya_Click);
+            // 
+            // dosya_yolu
+            // 
+            this.dosya_yolu.Location = new System.Drawing.Point(108, 125);
+            this.dosya_yolu.Name = "dosya_yolu";
+            this.dosya_yolu.Size = new System.Drawing.Size(170, 22);
+            this.dosya_yolu.TabIndex = 10;
+            this.dosya_yolu.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Dosya Yolu";
+            // 
+            // frame1
+            // 
+            this.frame1.FormattingEnabled = true;
+            this.frame1.Location = new System.Drawing.Point(108, 248);
+            this.frame1.Name = "frame1";
+            this.frame1.Size = new System.Drawing.Size(121, 24);
+            this.frame1.TabIndex = 8;
+            this.frame1.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Çerçeve";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // format1
+            // 
+            this.format1.FormattingEnabled = true;
+            this.format1.Location = new System.Drawing.Point(108, 208);
+            this.format1.Name = "format1";
+            this.format1.Size = new System.Drawing.Size(121, 24);
+            this.format1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Format";
+            // 
+            // height1
+            // 
+            this.height1.Location = new System.Drawing.Point(108, 78);
+            this.height1.Name = "height1";
+            this.height1.Size = new System.Drawing.Size(100, 22);
+            this.height1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Yükseklik";
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // sayi
+            // 
+            this.sayi.Location = new System.Drawing.Point(86, 622);
+            this.sayi.Name = "sayi";
+            this.sayi.Size = new System.Drawing.Size(183, 22);
+            this.sayi.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(85, 602);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(171, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Oynatılan Şuanki Görüntü";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1165, 728);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.sayi);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YUV Çözücü";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox width1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox frame1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox format1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox height1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dosya_yolu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Button goster;
+        private System.Windows.Forms.Button dosya;
+        private System.Windows.Forms.TextBox sayi;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ dd/dd/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace dd
+{
+    public partial class Form1 : Form
+    {
+        int frame_index = 0;
+        String[] pics;
+        int sayac = 0;
+        Bitmap greyBitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+            myInit();
+        }
+
+        private void myInit()
+        {
+            format1.Items.Add("4:4:4");
+            format1.Items.Add("4:2:2");
+            format1.Items.Add("4:2:0");
+
+            timer1.Interval = 50;
+            timer1.Tick += new EventHandler(ChangeImage);
+
+            pic.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Image img;
+            using (var bmpTemp = new Bitmap(pics[frame1.SelectedIndex]))
+            {
+                img = new Bitmap(bmpTemp);
+            }
+            pic.Image = img;
+            sayi.Text = pics[frame1.SelectedIndex];
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void goster_Click(object sender, EventArgs e){
+
+            try {
+                sayac = 0;
+                frame1.Items.Clear();
+                pic.Image = null;
+                sayi.Text = null;
+
+                int width = Convert.ToInt32(width1.Text);
+                int height = Convert.ToInt32(height1.Text);
+                String format = format1.SelectedItem.ToString(); 
+                int n_pixels = height * width;
+
+                int y_size = n_pixels;
+                int u_size;
+                int v_size;
+
+                if (format == "4:2:2"){
+                    u_size = n_pixels / 2;
+                    v_size = n_pixels / 2;
+                }
+
+                else if (format == "4:2:0"){
+                    u_size = n_pixels / 4;
+                    v_size = n_pixels / 4;
+                }
+                else{
+                    u_size = n_pixels;
+                    v_size = n_pixels;
+                }
+
+                int index = 0;
+
+                int[] y = new int[y_size];
+                int[] u = new int[u_size];
+                int[] v = new int[v_size];
+
+                int[,] GREY = new int[height, width];
+
+                greyBitmap = new Bitmap(width, height);
+                    
+                BinaryReader br;
+
+                String file = dosya_yolu.Text;
+
+                try{
+                    br = new BinaryReader(new FileStream(file, FileMode.Open));
+                }
+                catch (IOException){
+                    MessageBox.Show(" Dosya Açılamadı !");
+                    return;
+                }
+                frame_index = 0;
+
+                try{
+                    do{
+                        for (int i = 0; i < y_size; i++){
+                            y[i] = br.ReadByte();
+                            index++;
+                        }
+
+                        for (int i = 0; i < u_size; i++){
+                            u[i] = br.ReadByte();
+                            index++;
+                        }
+
+                        for (int i = 0; i < v_size; i++){
+                            v[i] = br.ReadByte();
+                            index++;
+                        }
+
+
+                        index = 0;
+                        for (int i = 0; i < height; i++){
+                            for (int j = 0; j < width; j++){
+                                GREY[i, j] = y[index];
+                                index++;
+                            }
+                        }
+
+                        for (int i = 0; i < height; i++){
+                            for (int j = 0; j < width; j++){
+                                greyBitmap.SetPixel(j, i, Color.FromArgb(GREY[i, j], GREY[i, j], GREY[i, j]));
+                            }
+                        }
+
+                        try
+                        {
+                            greyBitmap.Save("grey_" + frame_index + ".bmp", ImageFormat.Bmp);
+                            Console.WriteLine("Frame"+frame_index+" oluşturuldu !");
+                        }
+                        catch (Exception ex)
+                        {
+                            //Console.WriteLine("error\n" + ex);
+                        }
+                        
+                        frame_index++;
+                        frame1.Items.Add(frame_index);
+
+                    }while (br.BaseStream.Position != br.BaseStream.Length);
+
+                    pics = new String[frame_index];
+
+                    for (int i = 0; i < frame_index; i++){
+                        pics[i] = "grey_" + i + ".bmp";
+                    }
+                    
+                    timer1.Start();
+                    
+                    br.Close();   
+                }
+                catch (IOException d)
+                {
+                    MessageBox.Show("Görüntü Uygun Formatta Değildir !!");
+                }
+    
+            }
+            catch (FormatException a)
+            {
+                MessageBox.Show("Genişlik ve Yükseklik değerleri yalnızca rakamdan oluşmalıdır !");
+            }
+            catch (ArgumentException b)
+            {
+                MessageBox.Show("Dosya Yolu seçiniz !!");
+            }
+            catch (NullReferenceException c)
+            {
+                MessageBox.Show("Format Bilgisi Giriniz !");
+            }
+            
+        }
+        
+        private void dosya_Click(object sender, EventArgs e)
+        {
+            openFile.Filter = "YUV Dosyası | *.yuv";
+            openFile.ShowDialog();
+            dosya_yolu.Text = openFile.FileName;
+            dosya_yolu.Enabled = false;
+        }
+
+        private void ChangeImage(object sender, EventArgs e)
+        {
+            if (sayac != pics.Length){
+                Image img;
+                using (var bmpTemp = new Bitmap(pics[sayac]))
+                {
+                    img = new Bitmap(bmpTemp);
+                }
+                pic.Image = img;
+                sayi.Text = pics[sayac];
+                sayac++;
+            }
+            else
+            {
+                timer1.Stop();
+            }    
+        }
+
+        private void width1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
